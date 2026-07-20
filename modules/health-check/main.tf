6a2280ec5eac67913440e62d5f027f0a81863192
manjunath031984 <85352670+manjunath031984@users.noreply.github.com>
2026-07-20 22:41:36 +0530

    Add files via upload

---
+++ modules/health-check/main.tf
@@ -0,0 +1,12 @@
+resource "google_compute_health_check" "web_health_check" {
+  name                = "web-health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+
+  http_health_check {
+    port         = 80
+    request_path = "/"
+  }
+}
